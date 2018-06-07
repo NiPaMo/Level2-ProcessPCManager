5b640ad2ccfc35661ec05202b59b84f37a56422f
Nicholas Moyer <nicholas.moyer@aksteel.com>
2018-06-07 08:24:01 -0400

    Made all pings async with parallel multithreading

--- Tiny Pinger/MainWindow.xaml.cs
+++ Tiny Pinger/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,14 @@ namespace Tiny_Pinger
         Config config;
         PingReply reply;
 
+        int timeout = 100;
+        int interval = 60000;
+
         public MainWindow()
         {
             InitializeComponent();
 
-            Timer timer = new Timer(60000);
+            Timer timer = new Timer(interval);
             timer.Elapsed += OnTimedEvent;
             timer.AutoReset = true;
             timer.Enabled = true;
@@ -31,19 +35,7 @@ namespace Tiny_Pinger
             LoadConfig();
         }
 
-        private void OnTimedEvent(Object source, ElapsedEventArgs e)
-        {
-            this.Dispatcher.Invoke(() =>
-            {
-                int i = 0;
-
-                while (config.GetHostName(i).ToString().Length > 1)
-                {
-                    Ping(config.GetHostName(i).ToString());
-                    i++;
-                }
-            });
-        }
+       
 
         private void LoadConfig()
         {
@@ -88,29 +80,21 @@ namespace Tiny_Pinger
             PC20_HostName.Content = config.GetHostName(19);
             PC20_NickName.Content = config.GetNickName(19);
         }
-
-        private void Ping(string hostName)
+     
+        private async void PingAsync(string hostName)
         {
-            Ping pingSender = new Ping();
-            PingOptions options = new PingOptions
-            {
-                DontFragment = true
-            };
-
-            // Create a buffer of 32 bytes of data to be transmitted.
-            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-            byte[] buffer = Encoding.ASCII.GetBytes(data);
-            int timeout = 120;
+            Ping ping = new Ping();
 
             try
             {
-                reply = pingSender.Send(hostName, timeout, buffer, options);
+                reply = await ping.SendPingAsync(hostName, timeout);
                 ReplySucess(hostName);
             }
             catch (Exception e)
             {
                 ReplyFailure(hostName);
                 Console.WriteLine(e.Message);
+                Console.WriteLine(reply.Status);
             }
         }
 
@@ -119,140 +103,140 @@ namespace Tiny_Pinger
             if (hostName.Equals(PC1_HostName.Content.ToString()))
             {
                 PC1_Status.Fill = (Brush)converter.ConvertFromString("#145A32");
-                PC1_Data.Content = reply.Status.ToString() + "\n"
+                PC1_Data.Content = "Online\n"
                     + reply.Address.ToString() + "\n"
                     + reply.RoundtripTime.ToString() + " ms";
             }
             else if (hostName.Equals(PC2_HostName.Content.ToString()))
             {
                 PC2_Status.Fill = (Brush)converter.ConvertFromString("#145A32");
-                PC2_Data.Content = reply.Status.ToString() + "\n"
+                PC2_Data.Content = "Online\n"
                     + reply.Address.ToString() + "\n"
                     + reply.RoundtripTime.ToString() + " ms";
             }
             else if (hostName.Equals(PC3_HostName.Content.ToString()))
             {
                 PC3_Status.Fill = (Brush)converter.ConvertFromString("#145A32");
-                PC3_Data.Content = reply.Status.ToString() + "\n"
+                PC3_Data.Content = "Online\n"
                     + reply.Address.ToString() + "\n"
                     + reply.RoundtripTime.ToString() + " ms";
             }
             else if (hostName.Equals(PC4_HostName.Content.ToString()))
             {
                 PC4_Status.Fill = (Brush)converter.ConvertFromString("#145A32");
-                PC4_Data.Content = reply.Status.ToString() + "\n"
+                PC4_Data.Content = "Online\n"
                     + reply.Address.ToString() + "\n"
                     + reply.RoundtripTime.ToString() + " ms";
             }
             else if (hostName.Equals(PC5_HostName.Content.ToString()))
             {
                 PC5_Status.Fill = (Brush)converter.ConvertFromString("#145A32");
-                PC5_Data.Content = reply.Status.ToString() + "\n"
+                PC5_Data.Content = "Online\n"
                     + reply.Address.ToString() + "\n"
                     + reply.RoundtripTime.ToString() + " ms";
             }
             else if (hostName.Equals(PC6_HostName.Content.ToString()))
             {
                 PC6_Status.Fill = (Brush)converter.ConvertFromString("#145A32");
-                PC6_Data.Content = reply.Status.ToString() + "\n"
+                PC6_Data.Content = "Online\n"
                     + reply.Address.ToString() + "\n"
                     + reply.RoundtripTime.ToString() + " ms";
             }
             else if (hostName.Equals(PC7_HostName.Content.ToString()))
             {
                 PC7_Status.Fill = (Brush)converter.ConvertFromString("#145A32");
-                PC7_Data.Content = reply.Status.ToString() + "\n"
+                PC7_Data.Content = "Online\n"
                     + reply.Address.ToString() + "\n"
                     + reply.RoundtripTime.ToString() + " ms";
             }
             else if (hostName.Equals(PC8_HostName.Content.ToString()))
             {
                 PC8_Status.Fill = (Brush)converter.ConvertFromString("#145A32");
-                PC8_Data.Content = reply.Status.ToString() + "\n"
+                PC8_Data.Content = "Online\n"
                     + reply.Address.ToString() + "\n"
                     + reply.RoundtripTime.ToString() + " ms";
             }
             else if (hostName.Equals(PC9_HostName.Content.ToString()))
             {
                 PC9_Status.Fill = (Brush)converter.ConvertFromString("#145A32");
-                PC9_Data.Content = reply.Status.ToString() + "\n"
+                PC9_Data.Content = "Online\n"
                     + reply.Address.ToString() + "\n"
                     + reply.RoundtripTime.ToString() + " ms";
             }
             else if (hostName.Equals(PC10_HostName.Content.ToString()))
             {
                 PC10_Status.Fill = (Brush)converter.ConvertFromString("#145A32");
-                PC10_Data.Content = reply.Status.ToString() + "\n"
+                PC10_Data.Content = "Online\n"
                     + reply.Address.ToString() + "\n"
                     + reply.RoundtripTime.ToString() + " ms";
             }
             else if (hostName.Equals(PC11_HostName.Content.ToString()))
             {
                 PC11_Status.Fill = (Brush)converter.ConvertFromString("#145A32");
-                PC11_Data.Content = reply.Status.ToString() + "\n"
+                PC11_Data.Content = "Online\n"
                     + reply.Address.ToString() + "\n"
                     + reply.RoundtripTime.ToString() + " ms";
             }
             else if (hostName.Equals(PC12_HostName.Content.ToString()))
             {
                 PC12_Status.Fill = (Brush)converter.ConvertFromString("#145A32");
-                PC12_Data.Content = reply.Status.ToString() + "\n"
+                PC12_Data.Content = "Online\n"
                     + reply.Address.ToString() + "\n"
                     + reply.RoundtripTime.ToString() + " ms";
             }
             else if (hostName.Equals(PC13_HostName.Content.ToString()))
             {
                 PC13_Status.Fill = (Brush)converter.ConvertFromString("#145A32");
-                PC13_Data.Content = reply.Status.ToString() + "\n"
+                PC13_Data.Content = "Online\n"
                     + reply.Address.ToString() + "\n"
                     + reply.RoundtripTime.ToString() + " ms";
             }
             else if (hostName.Equals(PC14_HostName.Content.ToString()))
             {
                 PC14_Status.Fill = (Brush)converter.ConvertFromString("#145A32");
-                PC14_Data.Content = reply.Status.ToString() + "\n"
+                PC14_Data.Content = "Online\n"
                     + reply.Address.ToString() + "\n"
                     + reply.RoundtripTime.ToString() + " ms";
             }
             else if (hostName.Equals(PC15_HostName.Content.ToString()))
             {
                 PC15_Status.Fill = (Brush)converter.ConvertFromString("#145A32");
-                PC15_Data.Content = reply.Status.ToString() + "\n"
+                PC15_Data.Content = "Online\n"
                     + reply.Address.ToString() + "\n"
                     + reply.RoundtripTime.ToString() + " ms";
             }
             else if (hostName.Equals(PC16_HostName.Content.ToString()))
             {
                 PC16_Status.Fill = (Brush)converter.ConvertFromString("#145A32");
-                PC16_Data.Content = reply.Status.ToString() + "\n"
+                PC16_Data.Content = "Online\n"
                     + reply.Address.ToString() + "\n"
                     + reply.RoundtripTime.ToString() + " ms";
             }
             else if (hostName.Equals(PC17_HostName.Content.ToString()))
             {
                 PC17_Status.Fill = (Brush)converter.ConvertFromString("#145A32");
-                PC17_Data.Content = reply.Status.ToString() + "\n"
+                PC17_Data.Content = "Online\n"
                     + reply.Address.ToString() + "\n"
                     + reply.RoundtripTime.ToString() + " ms";
             }
             else if (hostName.Equals(PC18_HostName.Content.ToString()))
             {
                 PC18_Status.Fill = (Brush)converter.ConvertFromString("#145A32");
-                PC18_Data.Content = reply.Status.ToString() + "\n"
+                PC18_Data.Content = "Online\n"
                     + reply.Address.ToString() + "\n"
                     + reply.RoundtripTime.ToString() + " ms";
             }
             else if (hostName.Equals(PC19_HostName.Content.ToString()))
             {
                 PC19_Status.Fill = (Brush)converter.ConvertFromString("#145A32");
-                PC19_Data.Content = reply.Status.ToString() + "\n"
+                PC19_Data.Content = "Online\n"
                     + reply.Address.ToString() + "\n"
                     + reply.RoundtripTime.ToString() + " ms";
             }
             else if (hostName.Equals(PC20_HostName.Content.ToString()))
             {
                 PC20_Status.Fill = (Brush)converter.ConvertFromString("#145A32");
-                PC20_Data.Content = reply.Status.ToString() + "\n"
+                PC20_Data.Content = "Online\n"
                     + reply.Address.ToString() + "\n"
                     + reply.RoundtripTime.ToString() + " ms";
             }
@@ -263,103 +247,118 @@ namespace Tiny_Pinger
             if (hostName.Equals(PC1_HostName.Content.ToString()))
             {
                 PC1_Status.Fill = (Brush)converter.ConvertFromString("#641E16");
-                PC1_Data.Content = "Failure";
+                PC1_Data.Content = "Offline";
             }
             else if (hostName.Equals(PC2_HostName.Content.ToString()))
             {
                 PC2_Status.Fill = (Brush)converter.ConvertFromString("#641E16");
-                PC2_Data.Content = "Failure";
+                PC2_Data.Content = "Offline";
             }
             else if (hostName.Equals(PC3_HostName.Content.ToString()))
             {
                 PC3_Status.Fill = (Brush)converter.ConvertFromString("#641E16");
-                PC3_Data.Content = "Failure";
+                PC3_Data.Content = "Offline";
             }
             else if (hostName.Equals(PC4_HostName.Content.ToString()))
             {
                 PC4_Status.Fill = (Brush)converter.ConvertFromString("#641E16");
-                PC4_Data.Content = "Failure";
+                PC4_Data.Content = "Offline";
             }
             else if (hostName.Equals(PC5_HostName.Content.ToString()))
             {
                 PC5_Status.Fill = (Brush)converter.ConvertFromString("#641E16");
-                PC5_Data.Content = "Failure";
+                PC5_Data.Content = "Offline";
             }
             else if (hostName.Equals(PC6_HostName.Content.ToString()))
             {
                 PC6_Status.Fill = (Brush)converter.ConvertFromString("#641E16");
-                PC6_Data.Content = "Failure";
+                PC6_Data.Content = "Offline";
             }
             else if (hostName.Equals(PC7_HostName.Content.ToString()))
             {
                 PC7_Status.Fill = (Brush)converter.ConvertFromString("#641E16");
-                PC7_Data.Content = "Failure";
+                PC7_Data.Content = "Offline";
             }
             else if (hostName.Equals(PC8_HostName.Content.ToString()))
             {
                 PC8_Status.Fill = (Brush)converter.ConvertFromString("#641E16");
-                PC8_Data.Content = "Failure";
+                PC8_Data.Content = "Offline";
             }
             else if (hostName.Equals(PC9_HostName.Content.ToString()))
             {
                 PC9_Status.Fill = (Brush)converter.ConvertFromString("#641E16");
-                PC9_Data.Content = "Failure";
+                PC9_Data.Content = "Offline";
             }
             else if (hostName.Equals(PC10_HostName.Content.ToString()))
             {
                 PC10_Status.Fill = (Brush)converter.ConvertFromString("#641E16");
-                PC10_Data.Content = "Failure";
+                PC10_Data.Content = "Offline";
             }
             else if (hostName.Equals(PC11_HostName.Content.ToString()))
             {
                 PC11_Status.Fill = (Brush)converter.ConvertFromString("#641E16");
-                PC11_Data.Content = "Failure";
+                PC11_Data.Content = "Offline";
             }
             else if (hostName.Equals(PC12_HostName.Content.ToString()))
             {
                 PC12_Status.Fill = (Brush)converter.ConvertFromString("#641E16");
-                PC12_Data.Content = "Failure";
+                PC12_Data.Content = "Offline";
             }
             else if (hostName.Equals(PC13_HostName.Content.ToString()))
             {
                 PC13_Status.Fill = (Brush)converter.ConvertFromString("#641E16");
-                PC13_Data.Content = "Failure";
+                PC13_Data.Content = "Offline";
             }
             else if (hostName.Equals(PC14_HostName.Content.ToString()))
             {
                 PC14_Status.Fill = (Brush)converter.ConvertFromString("#641E16");
-                PC14_Data.Content = "Failure";
+                PC14_Data.Content = "Offline";
             }
             else if (hostName.Equals(PC15_HostName.Content.ToString()))
             {
                 PC15_Status.Fill = (Brush)converter.ConvertFromString("#641E16");
-                PC15_Data.Content = "Failure";
+                PC15_Data.Content = "Offline";
             }
             else if (hostName.Equals(PC16_HostName.Content.ToString()))
             {
                 PC16_Status.Fill = (Brush)converter.ConvertFromString("#641E16");
-                PC16_Data.Content = "Failure";
+                PC16_Data.Content = "Offline";
             }
             else if (hostName.Equals(PC16_HostName.Content.ToString()))
             {
                 PC17_Status.Fill = (Brush)converter.ConvertFromString("#641E16");
-                PC17_Data.Content = "Failure";
+                PC17_Data.Content = "Offline";
             }
             else if (hostName.Equals(PC16_HostName.Content.ToString()))
             {
                 PC18_Status.Fill = (Brush)converter.ConvertFromString("#641E16");
-                PC18_Data.Content = "Failure";
+                PC18_Data.Content = "Offline";
             }
             else if (hostName.Equals(PC16_HostName.Content.ToString()))
             {
                 PC19_Status.Fill = (Brush)converter.ConvertFromString("#641E16");
-                PC19_Data.Content = "Failure";
+                PC19_Data.Content = "Offline";
             }
             else if (hostName.Equals(PC16_HostName.Content.ToString()))
             {
                 PC20_Status.Fill = (Brush)converter.ConvertFromString("#641E16");
-                PC20_Data.Content = "Failure";
+                PC20_Data.Content = "Offline";
             }
+        }
+
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                int i = 0;
+
+                while (config.GetHostName(i).ToString().Length > 1)
+                {
+                    PingAsync(config.GetHostName(i).ToString());
+                    i++;
+                }
+            });
+
         }
 
         private void Ping_Click(object sender, RoutedEventArgs e)
@@ -368,7 +367,7 @@ namespace Tiny_Pinger
 
             while (config.GetHostName(i).ToString().Length > 1)
             {
-                Ping(config.GetHostName(i).ToString());
+                PingAsync(config.GetHostName(i).ToString());
                 i++;
             }
         }
