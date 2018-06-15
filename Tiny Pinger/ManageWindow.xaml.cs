dc13d45c3ae2cbbd1c4b4188bcb0efb91d4ff075
Nicholas Moyer <nicholas.moyer@aksteel.com>
2018-06-15 11:00:25 -0400

    Added ability to see all running processes

--- Tiny Pinger/ManageWindow.xaml.cs
+++ Tiny Pinger/ManageWindow.xaml.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Windows;
 using System.Management;
+using System.Collections.Generic;
 
 namespace Process_PC_Manager
 {
     public partial class ManageWindow : Window
     {
+        ManagementScope scope;
         Config config = new Config();
         PasswordPromptWindow prompt = new PasswordPromptWindow();
+
+        List<String> views = new List<string> { "Properties", "Processes" };
 
         string hostName;
 
@@ -24,8 +28,47 @@ namespace Process_PC_Manager
             {
                 HostName.Items.Add(name);
             }
-
             HostName.SelectedIndex = 0;
+
+            foreach (var view in views)
+            {
+                ViewBox.Items.Add(view);
+            }
+        }
+
+        private void PropertiesView()
+        {
+            // Query system for Operating System information
+            ObjectQuery query = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+
+            ManagementObjectCollection queryCollection = searcher.Get();
+            foreach (ManagementObject m in queryCollection)
+            {
+                // Display the remote computer information
+                Details.Text = "Computer Name\t:  " + m["CSName"] + "\n" +
+                               "Status       \t:  " + m["Status"] + "\n" +
+                               "OS Name      \t:  " + m["Caption"] + "\n" +
+                               "Service Pack \t:  " + m["CSDVersion"] + "\n" +
+                               "Version      \t:  " + m["Version"] + "\n" +
+                               "Build Number \t:  " + m["BuildNumber"] + "\n" +
+                               "Architecture \t:  " + m["OSArchitecture"] + "\n" +
+                               "Serial Number\t:  " + m["SerialNumber"] + "\n";
+            }
+        }
+
+        private void ProcessesView()
+        {
+            ObjectQuery query = new ObjectQuery("SELECT * FROM Win32_Process");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+
+            ManagementObjectCollection queryCollection = searcher.Get();
+            Details.Text = queryCollection.Count + " Active Processes\n";
+            foreach (ManagementObject m in queryCollection)
+            {
+                // Display the remote computer information
+                Details.Text += m["ProcessID"] + " :\t" + m["Name"] + "\n";
+            }
         }
 
         private void Connect_Click(object sender, RoutedEventArgs e)
@@ -43,26 +86,11 @@ namespace Process_PC_Manager
 
             try
             {
-                ManagementScope scope = new ManagementScope("\\\\" + hostName + ".maprocess.corp.aksteel.com\\root\\cimv2", options);
+                scope = new ManagementScope("\\\\" + hostName + ".maprocess.corp.aksteel.com\\root\\cimv2", options);
                 scope.Connect();
 
-                // Query system for Operating System information
-                ObjectQuery query = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
-                ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
-
-                ManagementObjectCollection queryCollection = searcher.Get();
-                foreach (ManagementObject m in queryCollection)
-                {
-                    // Display the remote computer information
-                    Details.Text = "Computer Name\t:  " + m["CSName"] + "\n" +
-                                   "Status       \t:  " + m["Status"] + "\n" +
-                                   "OS Name      \t:  " + m["Caption"] + "\n" +
-                                   "Service Pack \t:  " + m["CSDVersion"] + "\n" +
-                                   "Version      \t:  " + m["Version"] + "\n" +
-                                   "Build Number \t:  " + m["BuildNumber"] + "\n" +
-                                   "Architecture \t:  " + m["OSArchitecture"] + "\n" +
-                                   "Serial Number\t:  " + m["SerialNumber"];
-                }
+                ViewLabel.Visibility = Visibility.Visible;
+                ViewBox.Visibility = Visibility.Visible;
             }
             catch (UnauthorizedAccessException)
             {
@@ -73,6 +101,30 @@ namespace Process_PC_Manager
             {
                 Console.WriteLine(ex);
             }
+        }
+
+        private void ViewBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            switch (ViewBox.SelectedItem.ToString())
+            {
+                case "Properties":
+                    PropertiesView();
+                    break;
+
+                case "Processes":
+                    ProcessesView();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void HostName_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            Details.Text = "";
+            ViewLabel.Visibility = Visibility.Hidden;
+            ViewBox.Visibility = Visibility.Hidden;
         }
     }
 }
